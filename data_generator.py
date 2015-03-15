0197c8fc77223e0f45fbe3dc0cd4d402a61e4c26
ndt93 <thiennguyen1993@gmail.com>
2015-03-16 01:42:59 +0800

    implement data reading for learning driver

--- data_generator.py
+++ data_generator.py
@@ -20,4 +20,5 @@ if __name__ == "__main__":
 
             print " ".join(features)
 
-        print '#'
+        if game < num_games - 1:
+            print '#'
--- learning_driver.py
+++ learning_driver.py
@@ -1,6 +1,31 @@
 import numpy as np
+import sys
 
 from agent import Agent
 
+
+def read_data(filename):
+    data = []
+
+    with open(filename, 'r') as f:
+        game = []
+
+        for line in f:
+            if line[0] == '#':
+                data.append(np.array(game))
+                game = []
+            else:
+                state = line.split()
+                state = [int(x) for x in state]
+                game.append(state)
+
+    data.append(np.array(game))
+    return data
+
+
 if __name__ == "__main__":
-    agent2 = Agent()
+    data = read_data(sys.argv[1])
+
+    agent = Agent()
+    agent.set_data(data)
+    agent.set_learning_factor(0.5)
