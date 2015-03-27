57d16049c19c7e9929647ff6e02e9d5018972443
jinifief <jinified@gmail.com>
2015-03-27 10:51:47 +0800

    Edited learning_driver to automate learning process

--- scripts/learning_driver.py
+++ scripts/learning_driver.py
@@ -1,10 +1,29 @@
 import sys
+import os
 import random
+import subprocess
 
 import numpy as np
 
 from agent import Agent
 
+#CONSTANTS
+
+GAME_FILE = 'game_data'
+WEIGHT_FILE = 'weight_data'
+COUNT = 1
+WEIGHTS = [random.randint(-10, 10) for i in range(22)]
+INIT_VALS = '1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -5 -5 -5 -5 -5 -5 -5 -5 -5 -5 -5'
+NUM_GAMES = '100'
+PATH = os.path.abspath('../src')
+CMD = ['java', 'PlayerSkeleton', NUM_GAMES, INIT_VALS]
+
+def run_game():
+    global COUNT, CMD, FILE_NAME
+    print('Running game %s'%COUNT)
+    with open(GAME_FILE+str(COUNT), 'w') as f:
+        subprocess.call(CMD, stdout=f, cwd=PATH)
+    COUNT+=1
 
 def read_data(filename):
     data = []
@@ -25,12 +44,37 @@ def read_data(filename):
     return data
 
 
+'''
+arg1: number of cycle 
+arg2: start count
+arg3: initial weight_file
+'''
+
 if __name__ == "__main__":
-    data = read_data(sys.argv[1])
-
     agent = Agent()
-    agent.set_data(data)
-    agent.set_learning_factor(0.5)
-    agent.set_rt(np.array([random.randint(-10, 10) for i in range(22)]))
 
-    print agent.compute_next_rt()
+    subprocess.call(['javac', 'PlayerSkeleton.java'], cwd=PATH) #Compile java files
+    os.chdir('./data')
+
+    if len(sys.argv) == 4:
+        #Initialization step for COUNT and WEIGHTS
+        with open(sys.argv[3], 'r') as w:
+            weight = w.readline()
+            WEIGHTS = [float(x) for x in weight.split()]
+            INIT_VALS = ' '.join(["%.4f"%w for w in WEIGHTS])
+            COUNT = int(sys.argv[2])
+
+        for i in xrange(int(sys.argv[1])):
+            run_game()
+            data = read_data(GAME_FILE+str(COUNT-1))
+            agent.set_data(data)
+            agent.set_learning_factor(0.5)
+            agent.set_rt(np.array(WEIGHTS))
+            WEIGHTS = agent.compute_next_rt()
+            INIT_VALS = ' '.join(["%.4f"%w for w in WEIGHTS])
+            with open(WEIGHT_FILE+str(COUNT-1),'w') as f:
+                f.write(INIT_VALS)
+
+    else:
+        print "Usage: python learning_driver.py iterations start_count weight_file\n\
+        weight_file can be found in ./data"
